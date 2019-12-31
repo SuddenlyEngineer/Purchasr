d043e9993194e1326c35570e00c2028ac38f6dd8
SuddenlyEngineer <trhildebrand96@gmail.com>
2019-12-31 12:26:06 -0600

    Initial Commit
    Initial tests, JSON, and base class. Planning a TDD approach.

---
+++ Purchasr_mvp.py
@@ -0,0 +1,3 @@
+class Purchasr:
+    def __init__(self, user_wallet):
+        self.user_wallet = user_wallet
---
+++ Purchasr_mvp_test.py
@@ -0,0 +1,8 @@
+"""These are a series of tests to determine an MVP for Purchasr. These tests are subject to expand and change with time"""
+
+import unittest
+from Purchasr_mvp import Purchasr
+
+class PurchasrTest(unittest.TestCase):
+    def test_init(self):
+        pass
